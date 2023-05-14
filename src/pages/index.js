8711649b0cdcc7b62fb76904c44eb8aa98e54ae9
madrista1902 <124149904+madrista1902@users.noreply.github.com>
2023-05-14 16:26:15 +0200

    Delete src/pages directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export {default as Home} from './Home';
-export {default as adminSpace} from './adminSpace';
-export {default as AllProject} from './AllProject';
-export {default as Favourites} from './Favourites';
-export {default as ContactMe} from './ContactMe';
-export {default as AboutMe} from './AboutMe';
-
-
